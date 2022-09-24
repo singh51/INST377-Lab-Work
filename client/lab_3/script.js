c3f84a438b6d1a9cea5728d888d02102981ad2be
singh51 <112732074+singh51@users.noreply.github.com>
2022-09-23 22:42:10 -0400

    update

--- client/lab_3/script.js
+++ client/lab_3/script.js
@@ -28,25 +28,15 @@ function moveToNextSlide() {
   updateSlidePosition();
 }
 function moveToPrevSlide() {
-  // add your code in here for when you click the "prev" button
-  /*
-    add an if statement here that checks
-    if you're already at the first index position for an array
-    and if so, sets your slidePosition to the last slide position in totalSlides
-    if not, set the slidePosition to the current position minus one
-  */
-  if (slidePosition === totalSlides + 1) {
-    slidePosition = 0;
+  if (slidePosition === 0) {
+    slidePosition = totalSlides - 1;
   } else {
     slidePosition -= 1;
   }
+
   updateSlidePosition();
 }
 
-/*
-  These two functions have been assigned via "addEventListener"
-  to the elements accessed by the "querySelector" set to the class name on each
-*/
 document.querySelector('.next') // Get the appropriate element (<button class="next">)
   .addEventListener('click', () => { // set an event listener on it - when it's clicked, do this callback function
     console.log('clicked next'); // let's tell the client console we made it to this point in the script
